59b6af9b777e74bf461df7678307a03c69a3a1ac
GordilloXavi <xavier.gordillo@est.fib.upc.edu>
2019-09-26 17:56:03 +0200

    .

---
+++ S2/numeros.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+const int MAX_SIZE = 8;
+
+int esNumero (char *str);
+
+void
+main (int argc, char *argv[])
+{
+  char buf[80];
+  for (int i = 0; i < argc; i++)
+    {
+      if (i != 0)
+	{
+	  int n = esNumero (argv[i]);
+	  if (n)
+	    sprintf (buf, "%s es un numero\n", argv[i]);
+	  else
+	    sprintf (buf, "%s NO es un numero\n", argv[i]);
+	}
+		write(1,buf,strlen(buf));
+    }
+  return;
+}
+
+int
+esNumero (char *str)
+{
+  int i = 0;
+  while (str[i] != '\0')
+    {
+      if (i >= MAX_SIZE)
+	return 0;
+      if (str[i] < '0' || str[i] > '9')
+	{
+	  if (i == 0 && str[i] != '-' || i != 0)
+	    return 0;
+	}
+      ++i;
+    }
+  return 1;
+}
